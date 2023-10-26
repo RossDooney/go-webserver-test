33b9f562bc6e83e133f78b86da1e87e22d528749
RossDooney <rossdooney@gmail.com>
2023-10-26 11:21:49 +0100

    added check for token in header

--- handler_update_user.go
+++ handler_update_user.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/golang-jwt/jwt"
 )
+
+type jwtClaim struct {
+	ID     int `json:"id"`
+	Expiry int `json:"expires"`
+	jwt.StandardClaims
+}
 
 func (cfg *apiConfig) handlerUpdateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
@@ -19,6 +28,8 @@ func (cfg *apiConfig) handlerUpdateUser(w http.ResponseWriter, r *http.Request) 
 		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
 		return
 	}
+	jwt := r.Header.Get("Authorization")
+	fmt.Println(r.Header.Get(jwt))
 
 	user, err := cfg.DB.UpdateUser(params.Email, params.Password, params.ID)
 	if err != nil {
@@ -26,8 +37,12 @@ func (cfg *apiConfig) handlerUpdateUser(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	respondWithJSON(w, http.StatusCreated, User{
+	respondWithJSON(w, http.StatusOK, User{
 		ID:    user.ID,
 		Email: user.Email,
 	})
+}
+
+func handlerUpdateUser(jwt string) bool {
+	return false
 }
